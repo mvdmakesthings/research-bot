dff5aeab628a3cda2f36635e99b2be6195ccdad7
Michael VanDyke <michaeljamesvandyke@gmail.com>
2023-11-05 01:19:08 -0400

    abstract primary langchain functions and build basic ui

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![](./resources/img.png)
 
-A research bot using locally hosted LLMs, using Python and LangChain.
+A research bot using locally hosted LLMs, using LangChain and Ollama.
 
 - Start by running `ollama run mistral`
 
@@ -10,10 +10,19 @@ A research bot using locally hosted LLMs, using Python and LangChain.
 
 ## Getting Started
 - Install [Ollama](https://ollama.ai/) and begin running your desired model:
+    ```sh 
+    ollama run mistral
+    ```
+- Run main.py with Streamlit
+    ```sh
+    streamlit run main.py
+    ```
 
-    ```ollama run mistral```
-- Run Main.py
+## Technologies Used
+- [LangChain](https://python.langchain.com/docs/get_started/introduction)
+- [Ollama](https://github.com/jmorganca/ollama)
+- [Streamlit](https://docs.streamlit.io/library/api-reference)
 
 Helpful Resources
-- https://www.youtube.com/watch?v=lG7Uxts9SXs
-- https://www.youtube.com/watch?v=ogQUlS7CkYA
+- [https://www.youtube.com/watch?v=lG7Uxts9SXs]()
+- [https://www.youtube.com/watch?v=ogQUlS7CkYA]()
--- main.py
+++ main.py
@@ -1,48 +1,11 @@
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import LLMChain
-from dotenv import load_dotenv
-from langchain.llms import Ollama
-from langchain.prompts import PromptTemplate
+import langchain_helper as lch
+import streamlit as st
 
-load_dotenv()
+st.title("Pets name generator")
 
+animal_type = st.sidebar.selectbox("What is your pet?", ("Cow", "Cat", "Hen", "Dog", "Hamster", "Llama"))
+pet_color = st.sidebar.text_input(label=f"What color is your {animal_type}?", max_chars=15)
 
-def run():
-    """ The Initializer for this application.
-    """
-    generate_pet_name("cat", "black")
-
-def generate_pet_name(animal_type, pet_color):
-    """Generates a pet name based on Mistral 7B
-
-    Args:
-        animal_type (str): The animal type. ie, cat, dog, cow
-        pet_color (str): The color of your animal
-
-    Returns:
-        str: List of names for your pet.
-    """
-
-    llm = Ollama(
-        model="mistral",
-        num_gpu=120,
-        temperature=0,
-        callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-        verbose=False
-        )
-
-    prompt_template_name = PromptTemplate(
-        input_variables=['animal_type', 'pet_color'],
-        template="I have a {animal_type} pet and I want a cool name for it, it is {pet_color}. Suggest me five cool names for my pet."
-    )
-
-    name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
-    
-    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
-
-    return response
-
-# Main
-if __name__ == "__main__":
-    run()
+if pet_color:
+    response = lch.generate_pet_name(animal_type, pet_color)
+    st.text(response['pet_name'])
