265ce016266f929260537a5e8cb8cf9d79185dc6
Michael VanDyke <michaeljamesvandyke@gmail.com>
2023-11-05 12:39:11 -0500

    update ui and call scrape/summerize

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import langchain_helper as lch
-import streamlit as st
-import utilities.search as search
-import utilities.scrape as scrape
-from PIL import Image
+"""Main Module"""
 
+from PIL import Image
+import streamlit as st
+
+# import langchain_helper as lch
+# import utilities.search as search
+import utilities.scrape as scrape
+import utilities.summerize as summerize
 st.title("Alfred v1.0")
 
 header_image = Image.open('./resources/img.png')
 st.image(header_image, caption='A helpful monster that only thinks about research')
 website = st.text_input("Website to Scrape")
+prompt = st.text_input("Prompt Query")
 
-if website:
+if website and prompt:
     st.write("Scraping website: ", website)
-    site = scrape.scrape_website("Pow", website)
-    st.info(site)
+    site_content = scrape.scrape_website(prompt, website)
+    summary = summerize.summerize(prompt, site_content)
+    st.info(summary)
